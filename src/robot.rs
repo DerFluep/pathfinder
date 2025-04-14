20579c4d2e5dfef7848e9add048cb844350146bc
DerFluep <36232559+DerFluep@users.noreply.github.com>
2025-04-14 22:05:28 +0200

    add colision check

--- src/robot.rs
+++ src/robot.rs
@@ -68,12 +68,32 @@ impl Robot {
                     }
                 });
                 *distance = closest;
-                if *distance <= self.radius {
-                    self.sensor_collision = true;
-                } else {
-                    self.sensor_collision = false;
-                }
             });
+    }
+
+    fn check_collision(&mut self, room: &Vec<Line>) {
+        self.sensor_collision = false;
+        let pos_x = self.position.get_x();
+        let pos_y = self.position.get_y();
+        room.iter().for_each(|wall| {
+            let x1 = wall.get_a().get_x();
+            let x2 = wall.get_b().get_x();
+            let y1 = wall.get_a().get_y();
+            let y2 = wall.get_b().get_y();
+
+            let slope = (y2 - y1) / (x2 - x1);
+            let y_intercept = y1 - slope * x1;
+
+            let a = 1.0 + slope.powi(2);
+            let b = 2.0 * slope * (y_intercept - pos_y) - 2.0 * pos_x;
+            let c = pos_x.powi(2) + (y_intercept - pos_y).powi(2) - self.radius.powi(2);
+
+            let discriminant = b.powi(2) - 4.0 * a * c;
+
+            if discriminant >= 0.0 {
+                self.sensor_collision = true;
+            }
+        });
     }
 
     fn moving(&mut self, direction: &Direction) {
@@ -96,6 +116,9 @@ impl Robot {
     pub fn run(&mut self, room: &Vec<Line>, viewport: &mut Viewport) {
         // rotate to nearest wall
         'rotate: loop {
+            if viewport.get_input() {
+                break 'rotate;
+            };
             let mut min_dist = f32::MAX;
             let mut min_dist_dir = f32::MAX;
             self.lidar_scan(&room);
@@ -105,19 +128,21 @@ impl Robot {
                     min_dist_dir = num as f32;
                 }
             });
+            // 0.0 = robot forward direction
             if min_dist_dir == 0.0 {
                 break 'rotate;
             }
             self.rotate(&Rotation::Left);
-            if viewport.get_input() {
-                break;
-            };
             viewport.draw(&room, &self);
             ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
         }
 
         'moving: loop {
+            if viewport.get_input() {
+                break 'moving;
+            };
             self.lidar_scan(&room);
+            self.check_collision(&room);
             if self.sensor_collision {
                 break 'moving;
             }
